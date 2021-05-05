c6452e3e712e0b24c2758bc774cc228323dc1c90
Gaurav Pawar <gauravspawar9624@gmail.com>
2021-05-05 11:08:55 +0530

    code

--- src/HowItWorks.js
+++ src/HowItWorks.js
@@ -4,8 +4,8 @@ function HowItWorks() {
     return (
         <div className="HowItWorks">
             <h1>How It Works?</h1>
-            <p>1. <b>To Start the Service</b> fill all the required fields and click on <b>Start</b> button</p>
-            <p>2.<b>To Stop the Service</b> just enter email field and click on <b>Stop</b> button</p>
+            <p>1. <b>To Start the Service</b> fill all the required fields and click on <b>Start</b> button.</p>
+            <p>2.<b>To Stop the Service</b> just enter email field and click on <b>Stop</b> button.</p>
         </div>
     )
 }
